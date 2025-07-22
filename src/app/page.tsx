97181c76af0d010348e2cc73327c63cf42bb9b8b
Tahmid Azam <tahmid_azam@icloud.com>
2025-07-22 16:06:44 -0400

    fix: update link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-cols-2 gap-4 p-4">
       <Card
         label="Summer Training Log"
-        href="https://docs.google.com/spreadsheets/d/1Qnym4-vOcVDyV2yax6wfX5P2iY9ILoqRNxXGoT_GVaI/"
+        href="googlesheets://docs.google.com/spreadsheets/d/1Qnym4-vOcVDyV2yax6wfX5P2iY9ILoqRNxXGoT_GVaI/edit"
       />
     </div>
   );
