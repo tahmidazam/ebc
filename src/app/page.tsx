596fae0d97bfc96253bd047d914a549dd9035eaf
Tahmid Azam <tahmid_azam@icloud.com>
2025-07-22 15:59:52 -0400

    feat: add card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Card } from "@/components/card";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="grid grid-cols-2 gap-4 p-4">
+      <Card
+        label="Summer Training Log"
+        href="https://docs.google.com/spreadsheets/d/1Qnym4-vOcVDyV2yax6wfX5P2iY9ILoqRNxXGoT_GVaI/"
+      />
+    </div>
+  );
 }
---
+++ src/components/card.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export function Card({ label, href }: { label: string; href: string }) {
+  return (
+    <Link
+      target="_blank"
+      rel="noopener noreferrer"
+      href={href}
+      className="p-3 rounded-lg h-26 flex flex-col justify-end relative"
+      style={{
+        backgroundImage: "url('/mesh/0.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <h2 className="text-white font-medium">{label}</h2>
+    </Link>
+  );
+}
