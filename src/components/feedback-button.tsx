89bee2f29385c7be7747345f1e4fb2a083fa0a8c
Tahmid Azam <tahmid_azam@icloud.com>
2025-07-24 12:00:32 -0400

    feat: update feedback sheet to dialog

--- src/components/feedback-button.tsx
+++ src/components/feedback-button.tsx
@@ -1,55 +1,45 @@
-import { MessageCircleWarningIcon } from "lucide-react";
-import { Button } from "./ui/button";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerTitle,
-  DrawerTrigger,
-} from "./ui/drawer";
+import { LogOutIcon, MessageCircleWarningIcon } from "lucide-react";
 import Link from "next/link";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
+import { Button } from "./ui/button";
 
 export function FeedbackButton() {
   return (
-    <Drawer>
-      <DrawerTrigger asChild>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
         <Button size="icon" variant="outline">
           <MessageCircleWarningIcon />
         </Button>
-      </DrawerTrigger>
+      </AlertDialogTrigger>
 
-      <DrawerContent
-        className="h-full"
-        style={{
-          maxHeight: "calc(100vh - env(safe-area-inset-top))",
-        }}
-      >
-        <div className="flex flex-col pt-16 px-4 gap-4">
-          <DrawerTitle className="text-center text-3xl font-semibold tracking-tight">
-            Submit Feedback
-          </DrawerTitle>
-
-          <DrawerDescription className="text-center">
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Submit Feedback</AlertDialogTitle>
+          <AlertDialogDescription>
             Submit feature suggestions or report issues on GitHub. A GitHub
             account is required to open or comment on issues. Your feedback
             helps us build a smoother experience for every crew member.
-          </DrawerDescription>
-        </div>
-
-        <DrawerFooter className="pb-[env(safe-area-inset-bottom)]">
-          <Button asChild>
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction asChild>
             <Link href="x-safari-https://github.com/tahmidazam/ebc/issues/new">
               Continue to GitHub
             </Link>
-          </Button>
-
-          <DrawerClose asChild>
-            <Button variant="secondary">Cancel</Button>
-          </DrawerClose>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
